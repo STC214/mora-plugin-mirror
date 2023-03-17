eee2f3e4205cd3034ef16dcac694ea3d7b628d15
Rrrrrrray <28804884+Rrrrrrray@users.noreply.github.com>
2023-03-17 10:00:01 +0800

    fix：等级显示错误

--- model/breadShop.js
+++ model/breadShop.js
@@ -176,7 +176,7 @@ export default class breadShop extends moraBase {
       let rank = _.findIndex(ranks, (v) => _.isEqual(v.name, cache[qq].name));
       msg = `您在本群的排名为：${rank + 1}`;
     } else {
-      let ranksInfo = _.map(ranks, (v, idx) => `top${idx+1}：${v.name} Lv.${v.level}，拥有${this.stuff}${v.own}${this.unit}`);
+      let ranksInfo = _.map(ranks, (v, idx) => `top${idx+1}：${v.name} Lv.${v.Lv}，拥有${this.stuff}${v.own}${this.unit}`);
       msg = [`本群${this.stuff}排行TOP5`, ..._.take(ranksInfo, 5), '大家继续加油！'].join('\n');
     }
 
