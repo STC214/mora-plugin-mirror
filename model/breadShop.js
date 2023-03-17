f631eb05e3e85824c712cf1664b87a05f71acf4b
Rrrrrrray <28804884+Rrrrrrray@users.noreply.github.com>
2023-03-17 08:23:55 +0800

    fix：获取时间错误

--- model/breadShop.js
+++ model/breadShop.js
@@ -184,7 +184,7 @@ export default class breadShop extends moraBase {
 
   refreshTime(self, prop, msg) {
     let cd = _.get(self, prop.cd, 0);
-    if (Date().now() <= cd) {
+    if (+moment() <= cd) {
       return `还有 ${moment(cd).diff(moment(), 'minutes')} 分钟才能${msg}！`;
     } else {
       return false;
