34ca1c1d8b1a11f9bcb61a272707bd494f488a6a
Rrrrrrray <28804884+Rrrrrrray@users.noreply.github.com>
2023-10-14 00:48:51 +0800

    🐞 fix: 卡池显示bug

--- model/banner.js
+++ model/banner.js
@@ -134,7 +134,7 @@ export default class banner extends moraBase {
     rarity.forEach(i => {
       let _pool = []
       if (i.version && i.half) _pool.push(`所属版本：${i.version} ${i.half}`)
-      _pool.concat([
+      _pool = _.concat(_pool, [
         `卡池名称：${i.name.replace('|', '，')}`,
         `五星UP：${i.five.join('，')}`,
         `四星UP：${i.four.join('，')}`,
