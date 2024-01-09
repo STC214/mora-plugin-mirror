f36b6be5c821b4431ac204712c0d33a7c0b3b198
Rrrrrrray <28804884+Rrrrrrray@users.noreply.github.com>
2024-01-10 05:43:18 +0800

    🐞 fix: 旅行者

--- model/AkashaDB.js
+++ model/AkashaDB.js
@@ -1,111 +1,114 @@
-import moraBase from './moraBase.js';
-import fetch, { Headers } from 'node-fetch';
-import gsCfg from '../../genshin/model/gsCfg.js';
-import _ from 'lodash';
-import commonTools from './commonTools.js';
+import moraBase from './moraBase.js'
+import fetch, { Headers } from 'node-fetch'
+import gsCfg from '../../genshin/model/gsCfg.js'
+import _ from 'lodash'
+import commonTools from './commonTools.js'
 
-export default class AkashaDB extends moraBase{
+export default class AkashaDB extends moraBase {
   constructor (e) {
-    super(e);
-    this.model = 'AkashaDB';
+    super(e)
+    this.model = 'AkashaDB'
   }
-  
+
   async getData () {
-    let v = Math.random() * 10;
-    let abyss_url = `https://akashadata.com/static/data/abyss_total.js?v=${v}`;
+    let v = Math.random() * 10
+    let abyss_url = `https://akashadata.com/static/data/abyss_total.js?v=${v}`
     let role_url = `https://t.akashadata.com/xstatic/json/static_card_dict.js?v=${v}`
 
-    const meta = {
-      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.42"
-    }
-    this.myHeaders = new Headers(meta);
+    let role_res = await this.fetch_data(role_url)
+    if (!role_res) return false
+    await redis.set(`${this.prefix}role_dict`, JSON.stringify(role_res))
 
-    let role_res = await fetch(role_url, { headers: this.myHeaders, method: 'get', redirect: 'follow' });
-    if (!role_res.ok) {
-      return false;
-    }
-    role_res = await role_res.text();
-    role_res = JSON.parse(role_res.split('=')[1]);
-    await redis.set(`${this.prefix}role_dict`, JSON.stringify(role_res));
+    let abyss_res = await this.fetch_data(abyss_url)
+    if (!abyss_res) return false
 
-    let abyss_res = await fetch(abyss_url, { headers: this.myHeaders, method: 'get', redirect: 'follow' });
-    if (!abyss_res.ok) {
-      return false;
-    }
-    
-    let res = await abyss_res.text();
-    res = JSON.parse(res.split('=')[1]);
-    _.each(res.character_used_list, v => {
-      let role = gsCfg.getRole(v.name);
-      v.name = role.name;
-    });
+    _.each(abyss_res.character_used_list, v => {
+      let role = gsCfg.getRole(v.name)
+      v.name = role.name
+    })
 
-    res.team_list = this.transTeamList(res.team_list, role_res);
-    res.team_up_list = this.transTeamList(res.team_up_list, role_res);
-    res.team_down_list = this.transTeamList(res.team_down_list, role_res);
+    abyss_res.team_list = this.transTeamList(abyss_res.team_list, role_res)
+    abyss_res.team_up_list = this.transTeamList(abyss_res.team_up_list, role_res)
+    abyss_res.team_down_list = this.transTeamList(abyss_res.team_down_list, role_res)
 
-    await redis.set(`${this.prefix}abyss_total`, JSON.stringify(res));
-    return true;
+    await redis.set(`${this.prefix}abyss_total`, JSON.stringify(abyss_res))
+    return true
   }
-  
+
+  async fetch_data (url) {
+    const meta = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.42' }
+    let myHeaders = new Headers(meta)
+
+    let res = await fetch(url, { headers: myHeaders, method: 'get', redirect: 'follow' })
+    if (!res.ok) return false
+
+    if (_.includes(url, '.js')) {
+      res = await res.text()
+      res = JSON.parse(res.split('=')[1])
+    } else {
+      res = await res.json()
+    }
+
+    return res
+  }
+
   /**
   * 获取数据
   * @param {String} url 请求地址
   * @returns json数据
   */
   async getUsageRate (rarity) {
-    let data = await this.get_data();
+    let data = await this.get_data('abyss_total')
     let usageData = {
       abyssVersion: data.schedule_version_desc,
       updateTime: data.modify_time,
-      characterList: this.listFilter(data.character_used_list, rarity),
-    };
+      characterList: this.listFilter(data.character_used_list, rarity)
+    }
 
-    let render = await commonTools.getRenderData('Akasha', 'abyss', usageData);
-    return render;
+    let render = await commonTools.getRenderData('Akasha', 'abyss', usageData)
+    return render
   }
 
   async teamsPage (half, sort, page) {
-    let data = await this.get_data();
-    let teamList = data.team_list;
-    let _sort = 'mr';
+    let data = await this.get_data('abyss_total')
+    let teamList = data.team_list
+    let _sort = 'mr'
     let _count = 'ac'
     if (_.isEqual(half, '上半')) {
-      teamList = data.team_up_list;
-      _sort = 'umr';
-      _count = 'uc';
-    } else if (_.isEqual(half, '下半')){
-      teamList = data.team_down_list;
-      _sort = 'dmr';
-      _count = 'dc';
+      teamList = data.team_up_list
+      _sort = 'umr'
+      _count = 'uc'
+    } else if (_.isEqual(half, '下半')) {
+      teamList = data.team_down_list
+      _sort = 'dmr'
+      _count = 'dc'
     }
     if (_.isEqual(sort, '满星率')) {
-      teamList = _.orderBy(teamList, [_sort], ['desc']);
+      teamList = _.orderBy(teamList, [_sort], ['desc'])
     }
     _.each(teamList, v => {
-      v.count = v[_count];
-      v.fullstar = v[_sort];
-    });
+      v.count = v[_count]
+      v.fullstar = v[_sort]
+    })
 
-    teamList = _.chunk(teamList, 14);
+    teamList = _.chunk(teamList, 14)
     let teamData = {
       abyssVersion: data.schedule_version_desc,
       updateTime: data.modify_time,
-      page: page,
-      teamList: teamList[page - 1],
-    };
-
-    let render = await commonTools.getRenderData('Akasha', 'teams', teamData);
-    return render;
-  }
-
-  async get_data () {
-    let key = `${this.prefix}abyss_total`;
-    if (!await redis.exists(key)) {
-      await this.getData();
+      page,
+      teamList: teamList[page - 1]
     }
-    return JSON.parse(await redis.get(key));
+
+    let render = await commonTools.getRenderData('Akasha', 'teams', teamData)
+    return render
   }
+
+  async get_data (key) {
+    key = `${this.prefix}${key}`
+    if (!await redis.exists(key)) await this.getData()
+    return JSON.parse(await redis.get(key))
+  }
+
   /**
    * 筛选数据
    * @param {Array} list 原数据
@@ -113,28 +116,34 @@ export default class AkashaDB extends moraBase{
    * @returns 筛选后的数据
    */
   listFilter (list, rarity) {
-    if (rarity) {
-      list = _.filter(list, v => _.isEqual(v.rarity, rarity));
-    }
-    return list;
+    return rarity ? _.filter(list, v => _.isEqual(v.rarity, rarity)) : list
   }
 
   transTeamList (list, role_res) {
     _.each(list, v => {
-      v.tl = _.map(v.tl, i => {
-        if (_.isEqual(role_res[i].name, '旅行者')) {
-          return '主角';
-        } else {
-          return role_res[i].name;
-        }  
-      });
-      v.mr = Number(v.mr);
-      v.uc = Number(v.uc);
-      v.dc = Number(v.dc);
-      v.umr = Number(v.umr);
-      v.dmr = Number(v.dmr);
-    });
+      v.tl = _.map(v.tl, i => role_res[i].name)
+      v.mr = Number(v.mr)
+      v.uc = Number(v.uc)
+      v.dc = Number(v.dc)
+      v.umr = Number(v.umr)
+      v.dmr = Number(v.dmr)
+    })
 
-    return list;
+    return list
+  }
+
+  roleCheck (roles) {
+    if (!_.isEmpty(roles)) {
+      roles = roles.split(/,|，/g)
+      for (let i of roles) {
+        let role = gsCfg.getRole(i)
+        if (!role) {
+          this.e.reply(`角色：${i}无法查询，请检查角色名称'`)
+          return false
+        }
+        i = role.name
+      }
+    }
+    return roles || []
   }
 }
